dcc55f3b1e90a3260e7f8229dac7123eefec89e4
Delon Newman <delon.newman@gmail.com>
2012-03-10 22:38:20 -1000

    more replacing char* with string some formatting

--- src/piglatin.h
+++ src/piglatin.h
@@ -1,4 +1,4 @@
-typedef char *string;
+typedef char* string;
 
 typedef struct {
 	string  name;
@@ -11,13 +11,13 @@ typedef struct {
 	int     count;
 } Hash;
 
-Hash   hash_new();
-int    hash_add(hash, char*, char*);
-string hash_get_value(hash, char*);
-int    hash_has_value(hash, char*);
+Hash   Hash_new();
+int    Hash_add(hash, string, string);
+string Hash_get_value(hash, string);
+int    Hash_has_value(hash, string);
 
 string *translate(FILE*);
-Hash    create_dictionary(char**);
-int     starts_with_vowl(char*);
-int     starts_with_consonant(char*);
+Hash    create_dictionary(string*);
+int     starts_with_vowl(string);
+int     starts_with_consonant(string);
 string *parse_text(FILE*);
